cac59eee33324c6bc419e79381d8bc8329d67b02
Leroy Collazo <leroycollazo@hotmail.com>
2023-03-23 12:16:14 +0100

    initial commit

--- script.js
+++ script.js
@@ -5,3 +5,6 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+fetch('https://server.knotten.net/fakestore/')
+    .then(res => res.json())
+    .then(json => console.log(json))
