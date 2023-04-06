e88cd704ffe283bbfa089edd7e2a115337d32d9f
antfou <fougneranton@gmail.com>
2023-04-06 09:39:12 +0200

    added dymanic cards dynamically

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -8,6 +8,33 @@ fetch("https://fakestoreapi.com/products")
   }).catch(error => console.error(error)
   );
 
+const parent = document.getElementById("parent");
+
+for (let i = 1; i <= 20; i++) {
+  const card = document.createElement("div");
+  card.classList.add("col", "mb-5");
+
+  card.innerHTML = `
+    <div class="card h-100">
+      <img id="id${i}-image" class="card-img-top" alt="..." />
+      <div class="card-body p-4">
+        <div class="text-center">
+          <h5 id="id${i}-name" class="fw-bolder"></h5>
+          <p id="id${i}-price"></p>
+        </div>
+      </div>
+      <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+        <div class="text-center">
+          <a class="btn btn-outline-dark mt-auto" href="about.html" onclick="selectItem(${i})">About</a>
+        </div>
+      </div>
+    </div>
+  `;
+
+  parent.appendChild(card);
+}
+
+
 function render(tempArray) {
   for (let i = 1; i < tempArray.length + 1; i++) {
     document.getElementById("id" + i + "-name").innerHTML = tempArray.find(item => item.id === i).title
